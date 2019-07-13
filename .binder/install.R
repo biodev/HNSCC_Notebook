ab194e3a9798582ff31ddc2fe6ddc1890a6a4754
Ted Laderas <roshioshi@gmail.com>
2019-07-12 17:22:42 -0700

    update of binder folder with Dockerfile

--- .binder/install.R
+++ .binder/install.R
@@ -1,10 +1,56 @@
-install.packages('rBiopaxParser')
+pkgs <- c(
+  "OrganismDbi",
+  "ExperimentHub",
+  "Biobase",
+  "BiocParallel",
+  "graphite",
+  "biomaRt",
+  "Biostrings",
+  "BSgenome",
+  "ShortRead",
+  "IRanges",
+  "GenomicRanges",
+  "GenomicAlignments",
+  "GenomicFeatures",
+  "SummarizedExperiment",
+  "VariantAnnotation",
+  "DelayedArray",
+  "GSEABase",
+  "Gviz",
+  "AnnotationDbi", 
+  "reactome.db", 
+  "graphite", 
+  "HGNChelper", 
+  "graph",
+  "RBGL",
+  "Rgraphviz",
+  "rmarkdown",
+  "httr",
+  "knitr",
+  "BiocStyle"
+)
+
+ap.db <- available.packages(contrib.url(BiocManager::repositories()))
+ap <- rownames(ap.db)
+fnd <- pkgs %in% ap
+pkgs_to_install <- pkgs[fnd]
+
+ok <- BiocManager::install(pkgs_to_install, update=FALSE, ask=FALSE) %in% rownames(installed.packages())
+
+if (!all(fnd))
+  message("Packages not found in a valid repository (skipped):\n  ",
+          paste(pkgs[!fnd], collapse="  \n  "))
+if (!all(ok))
+  stop("Failed to install:\n  ",
+       paste(pkgs_to_install[!ok], collapse="  \n  "))
+
+suppressWarnings(BiocManager::install(update=TRUE, ask=FALSE))
+
 install.packages('roxygen2')
 install.packages('stringr')
 install.packages('rmarkdown')
 install.packages('knitr')
 install.packages('here')
-install.packages('packageDir')
 install.packages('roxygen2')
 install.packages('stringr')
 install.packages('DT')
